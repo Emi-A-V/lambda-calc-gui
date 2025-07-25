52854df491320a2a3aeb11b1d322013784a3eb06
not Unknuffig <emi.a.vo@proton.me>
2025-07-25 15:32:41 +0200

    Updated lambda-calc submodule.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,4 @@
 import {createApp} from 'vue'
-import { EventsOn } from '../wailsjs/runtime/runtime.js';
-import { Events } from '@wailsio/runtime';
 import App from './App.vue'
 import './style.css';
 
